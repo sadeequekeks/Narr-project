c5b3ba1cf2205f0720e0363f90149a374d49dae7
sadeequekeks <iazubair@atbu.edu.ng>
2020-09-28 22:58:36 +0100

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,106 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-card>
+    <v-card-title>
+      Uploaded Documents
+      <v-spacer></v-spacer>
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Search"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-title>
+    <v-data-table
+      :headers="headers"
+      :items="research"
+      :search="search"
+    ></v-data-table>
+  </v-card>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  export default {
+    data () {
+      return {
+        search: '',
+        headers: [
+          {
+            text: 'Title',
+            align: 'start',
+            sortable: false,
+            value: 'name',
+          },
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+          { text: 'Uploaded Date', value: 'date' },
+          { text: 'Time', value: 'time' },
+          { text: 'Action', value: 'action' },
+          
+        ],
+        research: [
+          {
+            name: 'The Role of cloud computing in educational sector',
+            date: '24/10/2020',
+            time: '54 minutes ago',
+            action: 4.0,
+          },
+          {
+            name: 'Implementing cloud computing storage in tertiary institution',
+            date: '31/09/2020',
+            time: '24 minutes ago',
+            action: 4.0,
+          },
+          {
+            name: 'Monitoring cyber attacks in institution',
+            date: '31/09/2020',
+            time: '24 minutes ago',
+            action: 4.0,
+          },
+          {
+            name: 'Configuring router using VLAN Protocol',
+            date: '31/09/2020',
+            time: '24 minutes ago',
+            action: 4.0,
+          },
+          {
+            name: 'Ethical hacking as a course in education',
+            date: '31/09/2020',
+            time: '24 minutes ago',
+            action: 4.0,
+          },
+          {
+            name: 'Cyber Security and protecting of banks information',
+            date: '31/09/2020',
+            time: '24 minutes ago',
+            action: 4.0,
+          },
+          {
+            name: 'Implementing Javascript in Backend using Node.js',
+            date: '31/09/2020',
+            time: '24 minutes ago',
+            action: 4.0,
+          },
+          {
+            name: 'Subnetting organisation computers and IP addressing',
+            date: '31/09/2020',
+            time: '24 minutes ago',
+            action: 4.0,
+          },
+          {
+            name: 'Rising insecurity in Nigeria',
+            date: '31/09/2020',
+            time: '24 minutes ago',
+            action: 4.0,
+          },
+          {
+            name: 'Cost benefit analysis of cloud computing in education',
+            date: '31/09/2020',
+            time: '24 minutes ago',
+            action: 4.0,
+          },
+        ],
+      }
+    },
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
